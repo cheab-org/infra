71bac9fc3c82bd107a5c071d93d6552ae0160775
Wiwatta Mongkhonchit <zentetsukenz@gmail.com>
2022-04-18 21:56:51 +0700

    Initialize k8s cluster

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as digitalocean from "@pulumi/digitalocean"
+import * as kubernetes from "@pulumi/kubernetes"
+import * as pulumi from "@pulumi/pulumi"
+
+const cluster = new digitalocean.KubernetesCluster("infra", {
+  region: digitalocean.Region.SGP1,
+  version: "latest",
+  autoUpgrade: true,
+  maintenancePolicy: {
+    day: "sunday",
+    startTime: "20:00"
+  },
+  nodePool: {
+    name: "default",
+    size: digitalocean.DropletSlug.DropletS1VCPU2GB,
+    nodeCount: 1,
+  }
+})
+
+export const kubeconfig = cluster.kubeConfigs[0].rawConfig
